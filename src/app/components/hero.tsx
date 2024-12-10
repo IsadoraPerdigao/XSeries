be360a92aaacabed1d8c13a133dace91d89bc38f
Isadora Perdigao - INFOR <isadora.perdigao@strataengenharia.com.br>
2024-12-10 16:56:49 -0300

    feat: creates hero style

---
+++ src/app/components/hero.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image"
+import Logo from "../../assets/Logo.svg"
+import ImageSelector from "../../assets/ImageTransition.svg"
+import DetailImage from "../../assets/DetailPhoto.svg"
+import styles from "../styles/hero.module.css"
+
+export const Hero = () => {
+    return (
+        <div className={styles.hero}>
+            <div className={styles.title}>
+                <h1>Sea La Vie</h1>
+                <p>Alibaug, Maharashtra</p>
+                <div className={styles.smallLogo}>
+                </div>
+            </div>
+            <Image src={ImageSelector} alt="Menu to check next photo" className={styles.menu}/>
+            <Image src={DetailImage} alt="Smaller house picture"/>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import styles from "./page.module.css";
 import { Button } from "./components/button";
 import { Header } from "./components/header";
+import { Hero } from "./components/hero";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Header />
+      <Hero />
       <main className={styles.main}>
-        <Header />
-        <Button text="Teste bem maior que o outro" disabled={false} bgColor="buttonSquare"/>
+        Teste
       </main>
     </div>
   );
